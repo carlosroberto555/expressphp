74e2f04fc356ac4255523e4c02e2de613272a81e
Carlos Roberto <carlos.junior@vertexgroup.com.br>
2018-02-27 16:14:25 -0300

    Fix Basic auth strategie

--- src/Auth/BasicAuth.php
+++ src/Auth/BasicAuth.php
@@ -3,29 +3,20 @@ namespace ExpressPHP\Auth;
 
 abstract class BasicAuth extends Auth {
 
-	public function use_strategie($req, $res) : bool
-	{
-		$headers = apache_request_headers();
-		return isset($headers['Authorization'])
-				&& strpos($headers['Authorization'], 'Basic') !== false;
+	public function use_strategie($req, $res) : bool {
+		return isset($_SERVER['PHP_AUTH_USER']);
 	}
 
 	public function is_authenticated() : bool {
 
-		if (!empty($this->user) || isset($_SERVER['PHP_AUTH_USER']))
-		{
-			$user = $_SERVER['PHP_AUTH_USER'];
-			$pass = $_SERVER['PHP_AUTH_PW'];
-
-			if ($this->authenticate($user, $pass)) {
-				return true;
-			} else {
-				http_response_code(401);
-				return false;
-			}
-		}
-		else
-		{
+		$user = $_SERVER['PHP_AUTH_USER'];
+		$pass = $_SERVER['PHP_AUTH_PW'];
+	
+		if (!empty($this->user)) {
+			return true;
+		} else if ($this->authenticate($user, $pass)) {
+			return true;
+		} else {
 			header('WWW-Authenticate: Basic');
 			http_response_code(401);
 			return false;
