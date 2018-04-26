2cf71a52e477524357ec3f4df5a711d2c07c3bde
Carlos Roberto <carlosrpj555@gmail.com>
2018-04-26 13:37:43 -0300

    permitir adicionar funções pelo midleware

--- src/Router/Response.php
+++ src/Router/Response.php
@@ -74,4 +74,12 @@ class Response {
 	public function end($message = null) {
 		exit ($message);
 	}
+
+	public function __call($method, $args)
+    {
+        if (isset($this->$method)) {
+            $func = $this->$method;
+            return call_user_func_array($func, $args);
+        }
+    }
 }
