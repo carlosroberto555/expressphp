cd04c01a78f3bbfca1752c5049ed9198665d41d6
Carlos Roberto <carlosrpj555@gmail.com>
2020-07-25 22:18:56 -0300

    add test file

---
+++ test/index.php
@@ -0,0 +1,16 @@
+<?php
+require '../vendor/autoload.php';
+
+use ExpressPHP\Express as app;
+
+$app = new app('/projects/expressphp');
+
+// Show a hello world
+// GET /
+$app->get('/', function ($req, $res) {
+  $res->send('Hello world!');
+});
+
+// Short phpinfo with arrow function (php 7.4)
+// ALL_METHODS /info
+$app->all('/info',  fn () => phpinfo());
