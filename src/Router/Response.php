b0e62efb4a9ceb5b05fef70df9cfc1487046b527
Carlos Roberto <carlosrpj555@gmail.com>
2020-09-06 17:25:28 -0300

    add fallback mimetype

--- src/Router/Response.php
+++ src/Router/Response.php
@@ -4,89 +4,113 @@ namespace ExpressPHP\Router;
 
 class Response
 {
-	public $mounturl;
+  public $mounturl;
 
-	public function __construct()
-	{
-		$this->mounturl = preg_replace('/\/\w+.php$/', '', $_SERVER['PHP_SELF']);
-	}
+  public function __construct()
+  {
+    $this->mounturl = preg_replace('/\/\w+.php$/', '', $_SERVER['PHP_SELF']);
+  }
 
-	public function send($resp)
-	{
-		echo $resp;
-	}
+  public function send($resp)
+  {
+    echo $resp;
+  }
 
-	public function sendFile($path)
-	{
-		// Get the mime-type
-		$type = mime_content_type($path);
-		$this->type($type);
+  public function sendFile($path)
+  {
+    // Get the mime-type
+    $type = get_mimetype($path);
+    $this->type($type);
 
-		// Disable auto download
-		$file = pathinfo($path, PATHINFO_BASENAME);
-		$this->header('Content-Disposition', "inline; filename=\"$file\"");
+    // Disable auto download
+    $file = pathinfo($path, PATHINFO_BASENAME);
+    $this->header('Content-Disposition', "inline; filename=\"$file\"");
 
-		// Headers e filetime
-		$filetime = filemtime($path);
-		$headers = getallheaders();
+    // Headers e filetime
+    $filetime = filemtime($path);
+    $headers = getallheaders();
 
-		// Cache control
-		$this->header('Last-Modified', gmdate('D, d M Y H:i:s \G\M\T', $filetime));
-		$this->header('Cache-Control', 'only-if-cached');
+    // Cache control
+    $this->header('Last-Modified', gmdate('D, d M Y H:i:s \G\M\T', $filetime));
+    $this->header('Cache-Control', 'only-if-cached');
 
-		// Se não tiver sido modificado, usa o cache
-		if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == $filetime)) {
-			$this->status(304);
-		} else {
-			$this->header('Content-transfer-encoding', 'binary');
-			$this->header('Content-length', filesize($path));
-			$this->status(200);
-			readfile($path);
-		}
+    // Se não tiver sido modificado, usa o cache
+    if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == $filetime)) {
+      $this->status(304);
+    } else {
+      $this->header('Content-transfer-encoding', 'binary');
+      $this->header('Content-length', filesize($path));
+      $this->status(200);
+      readfile($path);
+    }
 
-		$this->end();
-	}
+    $this->end();
+  }
 
-	public function json($resp)
-	{
-		$this->type('application/json');
+  public function json($resp)
+  {
+    $this->type('application/json');
 
-		if (defined('DEBUG') && constant('DEBUG')) {
-			echo json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-		} else {
-			echo json_encode($resp);
-		}
-	}
+    if (defined('DEBUG') && constant('DEBUG')) {
+      echo json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    } else {
+      echo json_encode($resp);
+    }
+  }
 
-	public function status($status = null)
-	{
-		return http_response_code($status);
-	}
+  public function status($status = null)
+  {
+    return http_response_code($status);
+  }
 
-	public function header($key, $value)
-	{
-		header("$key: $value");
-	}
+  public function header($key, $value)
+  {
+    header("$key: $value");
+  }
 
-	public function location($loc)
-	{
-		$this->header('Location', $this->mounturl . $loc);
-	}
+  public function location($loc)
+  {
+    $this->header('Location', $this->mounturl . $loc);
+  }
 
-	public function type($type)
-	{
-		$this->header('Content-Type', "$type; charset=utf-8");
-	}
+  public function type($type)
+  {
+    $this->header('Content-Type', "$type; charset=utf-8");
+  }
 
-	public function end($message = null)
-	{
-		exit($message);
-	}
+  public function end($message = null)
+  {
+    exit($message);
+  }
 
-	public function __call($method, $args)
-	{
-		if (isset($this->$method)) {
-			return call_user_func_array($this->$method, $args);
-		}
-	}
+  public function __call($method, $args)
+  {
+    if (isset($this->$method)) {
+      return call_user_func_array($this->$method, $args);
+    }
+  }
+}
+
+function get_mimetype(string $path)
+{
+  $exten = preg_replace('#.*\.(.*)$#', '$1', $path);
+  $type = mime_content_type($path);
+
+  // fallback if got wrong mime type
+  if ($exten !== 'html' && $type === 'text/html') {
+    switch ($exten) {
+      case 'js':
+        return 'application/javascript';
+      case 'json':
+        return 'application/json';
+      case 'css':
+        return 'text/css';
+      case 'html':
+        return 'text/html';
+      default:
+        return 'text/plain';
+    }
+  }
+
+  return $type;
 }
