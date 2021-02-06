fa31475c4288e66db0415c9e8d0d541876dcfff2
Carlos Roberto <carlosrpj555@gmail.com>
2021-02-06 01:48:20 -0300

    fix accept url with trailing slash

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -4,79 +4,80 @@ namespace ExpressPHP\Router;
 
 class Route
 {
-	private $mounturl;
+  private $mounturl;
 
-	public $path;
-	public $regexp;
-	public $param_regexp = '[^/]+';
+  public $path;
+  public $regexp;
+  public $param_regexp = '[^/]+';
 
-	public $params;
+  public $params;
 
-	public $result_url;
-	public $result_baseUrl;
-	public $result_path;
+  public $result_url;
+  public $result_baseUrl;
+  public $result_path;
 
-	public function __construct($mounturl, $path)
-	{
-		$this->path = $path;
-		$this->mounturl = $mounturl;
-		$this->params = new \stdClass;
+  public function __construct($mounturl, $path)
+  {
+    $this->path = $path;
+    $this->mounturl = $mounturl;
+    $this->params = new \stdClass;
 
-		preg_match_all('/\/:(\w+)/', $path, $params);
+    preg_match_all('/\/:(\w+)/', $path, $params);
 
-		foreach ($params[1] as $value) {
-			$this->params->$value = null;
-		}
-	}
+    foreach ($params[1] as $value) {
+      $this->params->$value = null;
+    }
+  }
 
-	public function matches($url, $exact = false)
-	{
-		$this->regexp = $this->regexp($this->path, $exact);
-		preg_match("#$this->regexp#", $this->mounturl . $url, $matches);
+  public function matches($url, $exact = false)
+  {
+    $this->regexp = $this->regexp($this->path, $exact);
+    preg_match("#$this->regexp#", $this->mounturl . $url, $matches);
 
-		if (!empty($matches)) {
-			$this->result_url = $matches[0];
-			$this->result_baseUrl = $matches[1];
-			$this->result_path = $matches[2];
-			$this->match_params($url);
+    if (!empty($matches)) {
+      $this->result_url = $matches[0];
+      $this->result_baseUrl = $matches[1];
+      $this->result_path = $matches[2];
+      $this->match_params($url);
 
-			return true;
-		}
+      return true;
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	/**
-	 * Match params route path to url
-	 * @param string $url The url to extract params
-	 */
-	private function match_params(string $url)
-	{
-		foreach ($this->params as $key => $value) {
-			// Create path regex to get param
-			$regexp = $this->param_regexp;
-			$path_regexp = preg_replace(["#/:$key(/|$)#", "#/:$regexp$1#"], ["/($regexp)/", "/$regexp"], $this->path);
+  /**
+   * Match params route path to url
+   * @param string $url The url to extract params
+   */
+  private function match_params(string $url)
+  {
+    foreach ($this->params as $key => $value) {
+      // Create path regex to get param
+      $regexp = $this->param_regexp;
+      $path_regexp = preg_replace(["#/:$key(/|$)#", "#/:$regexp$1#"], ["/($regexp)/", "/$regexp"], $this->path);
+      $safe_slash_url = rtrim($url, '/') . '/';
 
-			// Regex the url
-			preg_match("#$path_regexp#", $url, $matches);
+      // Regex the url
+      preg_match("#$path_regexp#", $safe_slash_url, $matches);
 
-			// Get the group 1 result
-			$this->params->$key = $matches[1];
-		}
-	}
+      // Get the group 1 result
+      $this->params->$key = $matches[1];
+    }
+  }
 
-	/**
-	 * Gera o regex para a rota atual
-	 */
-	private function regexp($path, $exact = false)
-	{
-		$path = preg_replace('/(:\w+)/', $this->param_regexp, $path);
-		$path = $path == '/' ? '' : $path;
+  /**
+   * Gera o regex para a rota atual
+   */
+  private function regexp($path, $exact = false)
+  {
+    $path = preg_replace('/(:\w+)/', $this->param_regexp, $path);
+    $path = $path == '/' ? '' : $path;
 
-		if ($exact) {
-			return "^$this->mounturl($path)(/?)$";
-		} else {
-			return "$this->mounturl($path)(/?.*)";
-		}
-	}
+    if ($exact) {
+      return "^$this->mounturl($path)(/?)$";
+    } else {
+      return "$this->mounturl($path)(/?.*)";
+    }
+  }
 }
